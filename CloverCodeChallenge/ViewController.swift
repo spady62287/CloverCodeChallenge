50f156f39926426def54ca7a2254374292afa5bc
Daniel Spady <danielspady@gmail.com>
2021-03-02 22:17:30 -0500

    Initial Commit

---
+++ CloverCodeChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CloverCodeChallenge
+//
+//  Created by Daniel Spady on 2021-03-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
